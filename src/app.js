55465a8bd4bd27de63611eadcd9bcf04dbc7e928
BrianVianaC <a340378836@my.uvm.edu.mx>
2024-03-09 18:51:14 -0600

    add header and update project to call page content

--- src/app.js
+++ src/app.js
@@ -1,9 +1,27 @@
-// Script to open and close sidebar
-function w3_open() {
-    document.getElementById("mySidebar").style.display = "block";
-  }
-  
-  function w3_close() {
-    document.getElementById("mySidebar").style.display = "none";
-  }
-  
+document.addEventListener("DOMContentLoaded", function() {
+
+  loadContent('../src/components/inicio/public/inicio.html');
+
+  // Manejar clics en los elementos del menú del encabezado
+  var menuItems = document.querySelectorAll('header nav ul li a');
+  menuItems.forEach(function(item) {
+      item.addEventListener('click', function(event) {
+          event.preventDefault();
+          var page = this.getAttribute('href');
+          loadContent(page); // Utiliza la ruta relativa desde el index.html
+      });
+  });
+});
+
+function loadContent(page) {
+  var contentContainer = document.getElementById("content");
+  contentContainer.innerHTML = ''; // Limpiar el contenedor antes de cargar el nuevo contenido
+
+  var objectElement = document.createElement('object');
+  objectElement.setAttribute('type', 'text/html');
+  objectElement.setAttribute('data', page);
+  objectElement.setAttribute('width', '100%');
+  objectElement.setAttribute('height', '100%');
+
+  contentContainer.appendChild(objectElement);
+}
