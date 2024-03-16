be3aa60048f6374b40cd6a28e8d5fbf8f4567322
BrianVianaC <a340378836@my.uvm.edu.mx>
2024-03-16 12:23:41 -0600

    add footer and pages

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
       item.addEventListener('click', function(event) {
           event.preventDefault();
           var page = this.getAttribute('href');
-          loadContent(page); // Utiliza la ruta relativa desde el index.html
+          loadContent(page); 
       });
   });
 });
@@ -17,11 +17,12 @@ function loadContent(page) {
   var contentContainer = document.getElementById("content");
   contentContainer.innerHTML = ''; // Limpiar el contenedor antes de cargar el nuevo contenido
 
-  var objectElement = document.createElement('object');
-  objectElement.setAttribute('type', 'text/html');
-  objectElement.setAttribute('data', page);
-  objectElement.setAttribute('width', '100%');
-  objectElement.setAttribute('height', '100%');
+  var iframeElement = document.createElement('iframe');
+  iframeElement.setAttribute('src', page);
+  iframeElement.setAttribute('frameborder', '0'); // Elimina el borde del iframe
+  iframeElement.style.width = '100%';
+  iframeElement.style.height = '100%';
+  iframeElement.style.overflow = 'auto'; // Agrega scroll si el contenido es más grande que el iframe
 
-  contentContainer.appendChild(objectElement);
+  contentContainer.appendChild(iframeElement);
 }
